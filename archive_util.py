c54e9e5c237a5408ecbc52399c59a6581fa3e8bf
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2021-07-10 18:13:46 -0400

    Added save optimizer

--- archive_util.py
+++ archive_util.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch import nn
+from torch.optim import optimizer
 
 import os
 
@@ -8,6 +9,11 @@ import os
 def save_model(model: nn.Module, epoch: int, model_name: str, dir_path: str):
     file_name = model_name + '_' + str(epoch) + ".pth"
     torch.save(model.state_dict(), os.path.join(dir_path, file_name))
+
+
+def save_optim(optim: optimizer, epoch: int, model_name: str, dir_path: str):
+    file_name = model_name + '_optim_' + str(epoch) + ".pth"
+    torch.save(optim.state_dict(), os.path.join(dir_path, file_name))
 
 
 def load_model(model: nn.Module, epoch: int, model_name: str, dir_path: str):
