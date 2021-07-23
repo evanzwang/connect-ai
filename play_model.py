a07ffc290c92efacce383254182adcde16c9a69e
alien_lover <59703666+alienloverlegit@users.noreply.github.com>
2021-07-22 20:52:59 -0400

    Play-testing against new weights

--- play_model.py
+++ play_model.py
@@ -25,7 +25,7 @@ def main(config_path: str):
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    path = "experiments/seven/seven.yml"
+    path = "experiments/eighth/eight.yml"
     epoch = 4500
     main(path)
 
